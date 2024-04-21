7113aae22732c0c5cb0fa09dc6cf204e51515873
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-04-21 21:20:56 +0200

    Add midi_sender

---
+++ autoguitar/midi_sender.py
@@ -0,0 +1,26 @@
+import random
+import time
+
+import mido
+from mido import Message
+
+# Open a virtual MIDI port to send messages.
+outport = mido.open_output("my_virtual_output", virtual=True)
+
+# minor blues scale
+scale = [60, 62, 63, 65, 66, 67, 72]
+
+try:
+    print("Sending MIDI notes.")
+    while True:
+        # Send a note on and note off message.
+        note = random.choice(scale)
+        outport.send(Message("note_on", note=note, velocity=64))
+        time.sleep(0.3 * random.randint(1, 4))
+        outport.send(Message("note_off", note=note, velocity=64))
+        time.sleep(0.01)
+except Exception as e:
+    print(e)
+finally:
+    # Close the port when done
+    outport.close()
