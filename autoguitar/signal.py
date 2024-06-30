b444cee1f0f06984f638fa71446094e952dc3015
Václav Volhejn <vaclav.volhejn@gmail.com>
2024-06-30 19:24:01 +0200

    Make Signal thread-safe

--- autoguitar/signal.py
+++ autoguitar/signal.py
@@ -1,3 +1,4 @@
+import threading
 from typing import Callable, Generic, TypeVar
 
 T = TypeVar("T")
@@ -6,19 +7,28 @@ T = TypeVar("T")
 class Signal(Generic[T]):
     def __init__(self):
         self._observers = set()
+        self._lock = threading.Lock()
 
     def subscribe(self, callback: Callable[[T], None]):
         """Registers a callback to the event."""
-        self._observers.add(callback)
+        with self._lock:
+            self._observers.add(callback)
 
     def unsubscribe(self, callback: Callable[[T], None]):
         """Unregisters a callback from the event."""
-        try:
-            self._observers.remove(callback)
-        except ValueError:
-            print("Warning: Tried to unsubscribe a callback that was not subscribed.")
+        with self._lock:
+            try:
+                self._observers.remove(callback)
+            except ValueError:
+                print(
+                    "Warning: Tried to unsubscribe a callback that was not subscribed."
+                )
 
     def notify(self, value: T):
         """Notifies all registered observers about an event."""
-        for observer in self._observers:
+        # Avoid holding the lock while calling the observers
+        with self._lock:
+            observers_snapshot = list(self._observers)
+
+        for observer in observers_snapshot:
             observer(value)
